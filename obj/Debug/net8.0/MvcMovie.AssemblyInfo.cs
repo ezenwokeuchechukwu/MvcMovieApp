8b5d60d7dc9c74d18e3bca202ecb82663668fb01
ezenwokeuchechukwu <ezenwokeuchechukwu@gmail.com>
2026-01-14 11:13:28 +0100

    Add full MvcMovie project

--- obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
+++ obj/Debug/net8.0/MvcMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3291fbef65cd454b4d19ff2966070b89775d8387")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32007c34df255f7660b5802bb33d7ef8b545e5ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvcMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
